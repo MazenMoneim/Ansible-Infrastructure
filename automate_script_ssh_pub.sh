8bb17b39bbb20394ee5693eb03b54da5c1b6953c
Your Name <you@example.com>
2024-05-10 15:49:40 +0000

    Second Commit with (when condition module)

--- automate_script_ssh_pub.sh
+++ automate_script_ssh_pub.sh
@@ -2,5 +2,5 @@
 
 for SERVERIP in $(cat /etc/ansible/hosts | grep '^[0-9]')
 do    
-    ssh -i key.pem ubuntu@$SERVERIP "echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBksDNRPoIKqH95Zt0z4oq0yCAFG/WtWYTt5z6h3ImKS ubuntu@ansible'  >> /home/ubuntu/.ssh/authorized_keys" 
+    ssh -i key.pem ubuntu@$SERVERIP "echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUPw+TmbNQlPRWo6CZR0cEzXb0oCL8d5RjrEYX0Cvad ubuntu@ansible'  >> /home/ubuntu/.ssh/authorized_keys" 
 done   
