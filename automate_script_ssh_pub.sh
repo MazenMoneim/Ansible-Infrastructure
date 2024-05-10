cf39ca9d847a87443fa31c887c1b2e0baf8fea1b
Your Name <you@example.com>
2024-05-10 17:39:59 +0000

    Delete and Add lines in the ntp.conf

--- automate_script_ssh_pub.sh
+++ automate_script_ssh_pub.sh
@@ -2,5 +2,5 @@
 
 for SERVERIP in $(cat /etc/ansible/hosts | grep '^[0-9]')
 do    
-    ssh -i key.pem ubuntu@$SERVERIP "echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUPw+TmbNQlPRWo6CZR0cEzXb0oCL8d5RjrEYX0Cvad ubuntu@ansible'  >> /home/ubuntu/.ssh/authorized_keys" 
+    ssh -i key.pem ubuntu@$SERVERIP "echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICy82xuNwV7C8mzu1c3zDKOwRpRUY+qVuw7XT6e6OMYx ubuntu@ansible'  >> /home/ubuntu/.ssh/authorized_keys" 
 done   
